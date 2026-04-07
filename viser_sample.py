6d9ac9e9506214db68ce661b1004d9f0de7cca12
chrismelville29 <cmelville113@gmail.com>
2026-04-07 17:18:36 -0500

    correcting color

--- viser_sample.py
+++ viser_sample.py
@@ -37,6 +37,7 @@ def cloudify_frame(cloud_handle, axis_handle, box_handle, frame_no):
 
     depth_img = cv2.imread(depth_prefix + f'{frame_no}.png', cv2.IMREAD_UNCHANGED)
     color_img = cv2.imread(color_prefix + f'{frame_no}.png')
+    color_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2RGB)
 
     _, rvec, tvec = reconstruct.detect_aruco_pose(color_img, intrinsic_matrix, None, 0.05)[0]
     quaternion, position = reconstruct.quatnpos_from_vector(tvec, rvec)
